1331dbcd9f56a9bc690f8514a46bc947f79c6b77
edakaraman <95571155+edakaraman@users.noreply.github.com>
2023-07-19 22:29:00 +0300

    e-commerce app
    e-commerce application interface design was made.

---
+++ App.js
@@ -0,0 +1,48 @@
+import {
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  Text,
+  FlatList,
+  Image,
+} from 'react-native';
+
+import ItemCard from './src/components/ItemCard';
+import item_data from './src/json/item_data.json';
+
+export default function App() {
+  const renderNews = ({ item }) => <ItemCard product={item} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.headerTitle}> PatikaStore </Text>
+      <TextInput style={styles.input} value={'Search..'} />
+      <FlatList
+        keyExtractor={(item) => item.id}
+        data={item_data}
+        renderItem={renderNews}
+        numColumns={2}></FlatList>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  headerTitle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 9,
+    color: '#393E46',
+    backgroundColor: '#EEEDED',
+  },
+});
---
+++ src/components/ItemCard/ItemCard.style.js
@@ -0,0 +1,34 @@
+import { StyleSheet, Dimensions } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 5,
+    padding: 12,
+    backgroundColor: '#EAEAEA',
+    borderRadius: 9,
+  },
+  item_img: {
+    height: Dimensions.get('window').height / 4,
+    borderRadius: 9,
+  },
+  item_title: {
+    fontWeight: 'bold',
+  },
+  item_price: {
+    color: 'gray',
+    textAlign:'left',
+  },
+  btn: {
+    backgroundColor: '#ff6000',
+    borderRadius: 5,
+    width: 40,
+    textAlign: 'center',
+  },
+  texts: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent:'space-between',
+    margin:3,
+  },
+});
